b2b56179e5fcbc8e37c67354c1cf69ef45846147
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-03-23 13:49:10 +0800

    显示所有元素的组件现已可用
    [Feature] 现在Tasks组件可以显示所有的list元素。

---
+++ src/views/Tasks.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Task title="To-Do" subtitle="Today's Tasks">
+            <template #>
+                <li v-for="e in list.getValues()">
+                    <h1>
+                        {{ e.title }}
+                    </h1>
+                    <p>
+                        {{ e.subtitle }}
+                    </p>
+                </li>
+            </template>
+        </Task>
+    </div>
+</template>
+
+<script setup>
+import Task from '../components/Task.vue'
+import useList from '../hooks/useList'
+
+const list = useList()
+</script>
+
+<style scoped>
+
+</style>
