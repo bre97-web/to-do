76e1800b856ba58419300e5b69a381d5befd6e58
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-03-24 14:58:39 +0800

    修改Me组件
    [Feature] 完善Me组件的模板。

---
+++ src/hooks/useTasks.js
@@ -0,0 +1,57 @@
+import { 
+    useList, useInnerList
+} from './useList'
+
+/**
+ * 初始化所有关于bin、focus、tasks的变量
+ * 这将作为全局对象并provide
+ */
+const taskList  = useList()
+const binList   = useInnerList('bin')
+const focusList = useInnerList('focus')
+
+
+
+/**
+ * 将元素e保存到bin，并将e从当前tasks中删除
+ * @param {Object} e 需要被移动到Bin的元素
+ */
+const moveToBin = (e) => {
+    taskList.remove(e)
+    focusList.remove(e)
+    binList.push(e)
+}
+/**
+ * 将元素e保存到tasks，并将e从当前bin中删除
+ * @param {Object} e 需要被移动到Tasks的元素
+ */
+const moveToTasks = (e) => {
+    taskList.push(e)
+    binList.remove(e)
+    focusList.remove(e)
+}
+const moveToFocus = (e) => {
+    taskList.remove(e)
+    focusList.push(e)
+}
+const removeTask  = (e) => taskList.remove(e)
+const removeFocus = (e) => focusList.remove(e)
+const removeBin   = (e) => binList.remove(e)
+
+
+/**
+ * 共享至组件
+ */
+export default function useTasks() {
+    return {
+        taskList,
+        focusList,
+        binList,
+        moveToBin,
+        moveToFocus,
+        moveToTasks,
+        removeBin,
+        removeFocus,
+        removeTask
+    }
+}
