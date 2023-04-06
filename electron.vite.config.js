860509de6a391b08d916bc8c58b48db8a71ecefe
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-04-06 13:34:27 +0800

    初始化electron相关配置

---
+++ electron.vite.config.js
@@ -0,0 +1,33 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@': resolve('./src/renderer/src')
+      }
+    },
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.startsWith('md-')
+          }
+        }
+      }),
+      vueJsx()
+    ],
+    build: {
+      outDir: './web-application'
+    }
+  }
+})
