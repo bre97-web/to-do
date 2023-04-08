6f143ee02507c1f329189e133ad0a987f998ce1d
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-04-08 20:15:19 +0800

    [Feature] 根据用户名称首字母的头像支持 (#4)
    * 分离App组件中的header成为Header组件
    
    [Fix]
    app中的header部分被拆分为Header组件。
    
    * Header组件中新增Avatar组件
    
    [Feature]
    新增Avatar组件。
    
    [Fix]
    修改Profile组件逻辑，与Avatar组件逻辑一致，头像使用名称首字母。
    
    * Header组件中新增Title组件
    
    * Update README.md

---
+++ src/renderer/src/components/header/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="topBar">
+        <div class="px-4 py-2 flex flex-row items-center justify-between w-full gap-2">
+            <!-- Web Title -->
+            <Title></Title>
+
+            <!-- Search input -->
+            <SearchInput :input="input" @setInput="setInput"></SearchInput>
+
+            <!-- Settings and other buttons -->
+            <Setting></Setting>
+
+            <!-- Users avatar-->
+            <Avatar></Avatar>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import Title from '@/components/header/Title.vue'
+import SearchInput from '@/components/header/SearchInput.vue'
+import Setting from '@/components/header/Setting.vue'
+import Avatar from '@/components/header/Avatar.vue'
+
+const props = defineProps({
+    input: {
+        type: String,
+        default: ''
+    }
+})
+const emits = defineEmits(['setInput'])
+const setInput = (value) => emits('setInput', value)
+</script>
---
+++ src/renderer/src/components/header/SearchInput.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="relative w-full flex items-center justify-center">
+        <i class="material-icons relative left-10">search</i>
+        <input
+            :value:="props.input"
+            type="text"
+            placeholder="Search"
+            @input="emits('setInput', $event.target.value)"
+        />
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    input: {
+        type: String,
+        default: ''
+    }
+})
+const emits = defineEmits(['setInput'])
+</script>
