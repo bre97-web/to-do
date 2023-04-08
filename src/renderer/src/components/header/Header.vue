2fd20c5d62506979a9092f004a2f63f55dc27ba3
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-04-08 19:50:08 +0800

    分离App组件中的header成为Header组件
    [Fix]
    app中的header部分被拆分为Header组件。

---
+++ src/renderer/src/components/header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="topBar">
+        <div class="px-4 py-2 flex flex-row items-center justify-between w-full gap-2">
+
+            <!-- Web Title -->
+            <h1>To-Do</h1>
+
+            <!-- Search input -->
+            <SearchInput :input="input" @setInput="setInput"></SearchInput>
+
+            <!-- Settings and other buttons -->
+            <Setting></Setting>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import SearchInput from '@/components/header/SearchInput.vue'
+import Setting from '@/components/header/Setting.vue'
+
+const props = defineProps({
+    input: {
+        type: String,
+        default: ''
+    }
+})
+const emits = defineEmits(['setInput'])
+const setInput = (value) => emits('setInput', value)
+</script>
