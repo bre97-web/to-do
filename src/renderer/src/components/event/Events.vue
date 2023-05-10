7e3d1a6aa6e9d577746e7589237d858705b7e3d6
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-05-10 10:59:44 +0800

    Create Events.vue

---
+++ src/renderer/src/components/event/Events.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <header>
+            <h1>
+                <slot></slot>
+            </h1>
+        </header>
+        <footer></footer>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
