4b13662289b5b087e45636a0db0057b25c2916dd
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-05-10 11:01:21 +0800

    Create Event.vue

---
+++ src/renderer/src/components/event/Event.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>
+            <slot></slot>
+        </h1>
+    </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/renderer/src/components/event/Events.vue
+++ src/renderer/src/components/event/Events.vue
@@ -1,14 +1,13 @@
 <template>
     <div>
-        <header>
-            <h1>
-                <slot></slot>
-            </h1>
-        </header>
-        <footer></footer>
+        <ul>
+            <li>
+                <Event></Event>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script lang="ts" setup>
-
+import Event from './Event.vue'
 </script>
