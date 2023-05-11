935166e182aa024d1ef3556a18aeb0cc903627f2
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-05-11 21:31:46 +0800

    修复头像名称显示不同步问题

--- src/renderer/src/assets/js/personal.ts
+++ src/renderer/src/assets/js/personal.ts
@@ -1,4 +1,4 @@
-import { reactive } from "vue";
+import { reactive, watch } from "vue";
 
 interface PersonalInterface {
     name: string
@@ -8,9 +8,12 @@ interface InfoInterface {
     set: ({}: PersonalInterface) => any
 }
 
+
 const PERSONAL = reactive<PersonalInterface>(JSON.parse(localStorage.getItem('bre97-web-todo-personal-info') as string) || {
     name: 'Click me to edit your info',
 })
+
+watch(PERSONAL, () => localStorage.setItem('bre97-web-todo-personal-info', JSON.stringify(PERSONAL)))
 
 function Info(): InfoInterface {
     const get = ():PersonalInterface => PERSONAL
