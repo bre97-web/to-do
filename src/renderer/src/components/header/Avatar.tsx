7748575518c6627565959b1bfb85fc7984830b26
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-05-11 21:32:44 +0800

    Update Avatar.tsx

--- src/renderer/src/components/header/Avatar.tsx
+++ src/renderer/src/components/header/Avatar.tsx
@@ -4,16 +4,13 @@ import personal from '@/assets/js/personal'
 const info = personal()
 
 const AvatarBoard = defineComponent({
-    render() {
-        return (
-            <div
-                class="flex-none rounded-full overflow-clip border w-10 h-10 flex items-center justify-center bg-off-base"
-            >
-                <h1>{ info.get().name }</h1>
-            </div>
-        )
-    }
+    render: () => (
+        <div
+            class="flex-none rounded-full overflow-clip border w-10 h-10 flex items-center justify-center bg-off-base"
+        >
+            <h1>{ info.get().name[0] }</h1>
+        </div>
+    )
 })
-// (e: UserName) => (
 
 export default AvatarBoard
