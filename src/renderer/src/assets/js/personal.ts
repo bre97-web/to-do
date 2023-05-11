a908ea408fb4f4cf59e62598db3b4365310b30fe
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-05-11 21:17:55 +0800

    Create personal.ts

---
+++ src/renderer/src/assets/js/personal.ts
@@ -0,0 +1,22 @@
+import { reactive } from "vue";
+
+interface PersonalInterface {
+    name: string
+}
+interface InfoInterface {
+    get: () => PersonalInterface
+}
+
+const PERSONAL = reactive<PersonalInterface>(JSON.parse(localStorage.getItem('bre97-web-todo-personal-info') as string) || {
+    name: 'Click me to edit your info',
+})
+
+function Info(): InfoInterface {
+    const get = () => PERSONAL
+
+    return {
+        get
+    }
+}
+
+export default Info
