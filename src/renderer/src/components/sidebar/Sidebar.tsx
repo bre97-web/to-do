46869b225400be8ab0803072eb7ebd0a37c2a605
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-05-12 18:25:20 +0800

    Create Sidebar.ts

---
+++ src/renderer/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,16 @@
+
+
+
+const Sidebar = () => (
+    <div class={'fixed hidden lg:block left-0 pl-4 w-44 h-96'}>
+        <div>
+            <h1>Sidebar</h1>
+            <ul class={'flex flex-col'}>
+                <li><i>Side bar contnet is coming</i></li>
+            </ul>
+        </div>
+    </div>
+)
+
+
+export default Sidebar
