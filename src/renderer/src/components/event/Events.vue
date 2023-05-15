4cd98729adc8155f567c1344393557d61ef48c73
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-05-15 19:59:19 +0800

    Event组件改写为tsx

--- src/renderer/src/components/event/Events.vue
+++ src/renderer/src/components/event/Events.vue
@@ -2,16 +2,16 @@
     <div class="eventsSurface">
         <ul class="events">
             <li v-for="e in events.getAll()" :key="e.index">
-                <Event :event="e"></Event>
+                <Event :msg="e.msg" :isRollback="e.isRollback"></Event>
             </li>
         </ul>
     </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="tsx" setup>
 import { getCurrentInstance } from 'vue'
 import { EventsInterface } from '@/hooks/useEvent';
-import Event from './Event.vue'
+import { Event } from './lib/Event'
 
 const events:EventsInterface = getCurrentInstance()?.appContext.config.globalProperties.$events
 </script>
