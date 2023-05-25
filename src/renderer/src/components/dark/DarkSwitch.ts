a7a5e6e653001803455034ac09c5f11df7c1ffdb
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-05-25 16:54:01 +0800

    Update DarkSwitch.ts

--- src/renderer/src/components/dark/DarkSwitch.ts
+++ src/renderer/src/components/dark/DarkSwitch.ts
@@ -1,6 +1,5 @@
 import useTheme from '@/hooks/useTheme'
 import { LitElement, html } from 'lit'
-// import { customElement } from 'lit/decorators.js'
 
 import '@material/web/switch/switch'
 
@@ -11,7 +10,6 @@ interface Switch {
 
 const theme = useTheme()
 
-// @customElement('lit-dark-switch')
 class DarkSwitch extends LitElement {
 
     private selected = theme.getCurrent().isDark
