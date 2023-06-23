1a53469b5f5dcdf974e58aa5faf001d29b0c2c92
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-06-23 12:33:17 +0800

    Update Avatar.tsx

--- src/renderer/src/components/setting/lib/Avatar.tsx
+++ src/renderer/src/components/setting/lib/Avatar.tsx
@@ -6,7 +6,7 @@ const info = personal()
 const AvatarBoard = defineComponent({
     render: () => (
         <div
-            class="flex-none rounded-full overflow-clip border w-10 h-10 flex items-center justify-center bg-off-base"
+            class="flex-none rounded-full overflow-clip border w-10 h-10 flex items-center justify-center"
         >
             <h1>{ info.get().name[0] }</h1>
         </div>
