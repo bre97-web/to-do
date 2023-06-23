bdc0c3cc1f3b18427b1c0c9cef99dc3800e2837f
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-06-23 12:55:59 +0800

    提供新的md配色

--- src/renderer/src/views/dashboard/Dashboard.vue
+++ src/renderer/src/views/dashboard/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative w-full">
+    <div class="relative w-full bg-background">
         <div class="relative flex flex-col md:flex-row h-screen">
             <div class="relative border w-full md:max-w-xs h-auto md:h-screen p-2 md:p-8">
                 <nav class="flex justify-end md:absolute md:bottom-4">
