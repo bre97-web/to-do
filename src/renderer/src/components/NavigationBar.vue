999c35cfb7ed0f1c681cb13b6b2c17731646ccc4
Bre 97 <by.sgatn3@Gmail.com>
2023-07-06 12:16:44 +0800

    fix the index error

---
+++ src/renderer/src/components/NavigationBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <md-navigation-bar :active-index="currentIndex">
+        <md-navigation-tab v-for="e in routerList" :key="e.url" :label="e.label" @click="router.push(e.url)">
+        </md-navigation-tab>
+    </md-navigation-bar>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const currentUrl = computed(() => router.currentRoute.value.path)
+const currentIndex = computed(() => {
+    for(let index in routerList) {
+        if(routerList[index].url === currentUrl.value) {
+            return index
+        }
+    }
+    return 0
+})
+const routerList = [
+    {
+        url: '/home',
+        label: 'Home'
+    },
+    {
+        url: '/goals',
+        label: 'Goals'
+    },
+    {
+        url: '/group',
+        label: 'Group'
+    },
+    {
+        url: '/me',
+        label: 'Profile'
+    },
+]
+</script>
+
+<style scoped></style>
