93b255f34504373ae81d4fdbb44cb5324a271581
Bre 97 <by.sgatn3@Gmail.com>
2023-07-06 08:48:50 +0800

    changed the snackbar css and dom

--- src/renderer/src/components/EventSnackbar.vue
+++ src/renderer/src/components/EventSnackbar.vue
@@ -1,12 +1,10 @@
 <template>
-    <div class="eventsSurface">
-        <ul class="events">
-            <li v-for="e in events.getAll()" :key="e.index">
-                <div class="event surfaceContainer">
-                    <h1>{{ e.msg }}</h1>
-                    <div :class="{ invisible: !e.isRollback }">
-                        <md-text-button @click.once="() => e.fn()">Undo</md-text-button>
-                    </div>
+    <div class="fixed z-50 bottom-24 lg:bottom-5 left-4 w-1/2 md:w-52 xl:w-96">
+        <ul class="flex flex-col w-full gap-2">
+            <li v-for="e in events.getAll()" :key="e.index" class="scoped-eventAnimate opacity-0 px-4 py-2 rounded-md shadow flex flex-row items-center justify-between surfaceContainer">
+                <h1>{{ e.msg }}</h1>
+                <div :class="{ invisible: !e.isRollback }">
+                    <md-text-button @click.once="() => e.fn()">Undo</md-text-button>
                 </div>
             </li>
         </ul>
@@ -20,3 +18,27 @@ import '@material/web/button/text-button'
 
 const events: EventsInterface = getGlobalEvents().get()
 </script>
+
+
+<style scoped>
+.scoped-eventAnimate {
+    animation: 5s eventAnimation;
+}
+
+@keyframes eventAnimation {
+    from {
+        @apply opacity-0 block;
+    }
+
+    20% {
+        @apply opacity-100;
+    }
+
+    80% {
+        @apply opacity-100;
+    }
+
+    to {
+        @apply opacity-0;
+    }
+}</style>
