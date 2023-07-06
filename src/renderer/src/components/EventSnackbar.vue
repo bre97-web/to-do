5fd8901a8aa5d8152189f14a35ba509d8996f369
Bre 97 <by.sgatn3@Gmail.com>
2023-07-06 12:32:06 +0800

    Update EventSnackbar.vue

--- src/renderer/src/components/EventSnackbar.vue
+++ src/renderer/src/components/EventSnackbar.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="fixed z-50 bottom-24 lg:bottom-5 left-4 w-1/2 md:w-52 xl:w-96">
         <ul class="flex flex-col w-full gap-2">
-            <li v-for="e in events.getAll()" :key="e.index" class="scoped-eventAnimate opacity-0 px-4 py-2 rounded-md shadow flex flex-row items-center justify-between surfaceContainer">
-                <h1>{{ e.msg }}</h1>
-                <div :class="{ invisible: !e.isRollback }">
-                    <md-text-button @click.once="() => e.fn()">Undo</md-text-button>
-                </div>
-            </li>
+            <TransitionGroup>
+                <li v-for="e in events.getAll()" :key="e.index" class="px-4 py-2 rounded-md shadow flex flex-row items-center justify-between surfaceContainer">
+                    <h1>{{ e.msg }}</h1>
+                    <div :class="{ invisible: !e.isRollback }">
+                        <md-text-button @click.once="() => e.fn()">Undo</md-text-button>
+                    </div>
+                </li>
+            </TransitionGroup>
         </ul>
     </div>
 </template>
@@ -21,24 +23,13 @@ const events: EventsInterface = getGlobalEvents().get()
 
 
 <style scoped>
-.scoped-eventAnimate {
-    animation: 5s eventAnimation;
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.5s ease;
 }
 
-@keyframes eventAnimation {
-    from {
-        @apply opacity-0 block;
-    }
-
-    20% {
-        @apply opacity-100;
-    }
-
-    80% {
-        @apply opacity-100;
-    }
-
-    to {
-        @apply opacity-0;
-    }
-}</style>
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+</style>
