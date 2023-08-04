bc82f990da0be13ec3d2f9ba6149f9c9d576e03d
bre97-web <by.sgatn3@gmail.com>
2023-08-04 18:14:31 +0800

    update css

--- src/renderer/src/views/test.vue
+++ src/renderer/src/views/test.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-
+        <h1>Headline 1</h1>
+        <h2>Headline 2</h2>
+        <h3>Headline 3</h3>
+        <h4>Headline 4</h4>
+        <h5>Headline 5</h5>
+        <h6>Headline 6</h6>
+        <subtitle1>Headline 6</subtitle1>
+        <subtitle2>Headline 6</subtitle2>
+        <body1>Headline 6</body1>
+        <body2>Headline 6</body2>
     </div>
 </template>
 
