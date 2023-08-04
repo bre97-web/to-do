e8e109c3c5d024ab28271699de5c85d5f058f74a
bre97-web <by.sgatn3@gmail.com>
2023-08-04 10:08:16 +0800

    useCollection

--- src/renderer/src/hooks/useTask.ts
+++ src/renderer/src/hooks/useTask.ts
@@ -13,13 +13,15 @@ export type Steps = Step[]
 export type Index = number
 export type Date = string
 export type Type = 'task' | 'goal' | 'collection'
+export type FromCollection = string
+export type ProgressStatus = 'processing' | 'pinned' | 'done'
 
 /**
- * 一个Item相当于一个Todo（或Goal）
- * 请使用useItem(...)来创建一个Item类型的对象，useItem已经提供了部分缺省参数
+ * 一个Task相当于一个Todo（或Goal）
+ * 请使用useTask(...)来创建一个Task类型的对象，useTask已经提供了部分缺省参数
  */
 export interface Task {
-    // Item最终显示的标题
+    // Task最终显示的标题
     title: Title
 
     /**
@@ -28,13 +30,13 @@ export interface Task {
     subtitle: Subtitle
 
     /**
-     * 一般不会直接显式note属性，它仅仅用于用户对一个Item的详细说明
+     * 一般不会直接显式note属性，它仅仅用于用户对一个Task的详细说明
      */
     note: Note
 
     /**
      * 谨慎修改
-     * tags是一个重要的属性，它用于给自身打上标记，在关于Item显示的组件中经常使用tags
+     * tags是一个重要的属性，它用于给自身打上标记，在关于Task显示的组件中经常使用tags
      */
     tags: Tags
 
@@ -44,22 +46,22 @@ export interface Task {
     steps: Steps
 
     /**
-     * 创建Item时的可选项
-     * index是一个Item的唯一标识符，它可以用来作为列表的key
-     * 此属性不需要由用户提供，也不需要在创建Item时手动赋值，它的值是当前的UNIX时间戳
+     * 创建Task时的可选项
+     * index是一个Task的唯一标识符，它可以用来作为列表的key
+     * 此属性不需要由用户提供，也不需要在创建Task时手动赋值，它的值是当前的UNIX时间戳
      * 在store中调用push时会自动赋值
      */
     index: Index
 
     /**
-     * 创建Item时的可选项
+     * 创建Task时的可选项
      * 创建日期
      * （在store中调用push时会自动赋值）
      */
     createdDate: Date
 
     /**
-     * 创建Item时的可选项
+     * 创建Task时的可选项
      * 它由用户提供，如果用户没有提供此参数，此参数为null
      * 目标日期（由用户指定指定的需要完成的日期）
      * （在store中调用push时会自动赋值）
@@ -70,6 +72,17 @@ export interface Task {
      * [目前作用未知]
      */
     type: Type
+
+    /**
+     * 如果type为collection，fromCollection应该不为null
+     */
+    fromCollection?: FromCollection
+
+    /**
+     * 任务的处理进度
+     * processing为处理中，done为处理完成，pinned为已标记（固定，它的处理逻辑同processing）
+     */
+    progressStatus: ProgressStatus
 }
 export type Tasks = Task[]
 
@@ -85,7 +98,7 @@ const createIndex = (): number => useIndex()
 const createDate = (): string => useDate()
 
 /**
- * 返回一个Item类型的对象
+ * 返回一个Task类型的对象
  * 需要指定title,剩余参数提供了缺省值
  */
 export function useTask({
@@ -95,7 +108,6 @@ export function useTask({
     tags = [],
     steps = [],
     targetDate = null,
-    type = 'task'
 }: {
     title: string
     subtitle?: string
@@ -103,7 +115,6 @@ export function useTask({
     tags?: Tags
     steps?: Steps
     targetDate?: Date | null
-    type?: Type
 }): Task {
     return {
         title: title,
@@ -114,7 +125,40 @@ export function useTask({
         index: createIndex(),
         createdDate: createDate(),
         targetDate: targetDate,
-        type: type
+        type: 'task',
+        progressStatus: 'processing',
+    }
+}
+
+export function useCollection({
+    fromCollection,
+    title,
+    subtitle = '',
+    note = '',
+    tags = [],
+    steps = [],
+    targetDate = null,
+}: {
+    fromCollection: string
+    title: string
+    subtitle?: string
+    note?: string
+    tags?: Tags
+    steps?: Steps
+    targetDate?: Date | null
+}): Task {
+    return {
+        title: title,
+        subtitle: subtitle,
+        note: note,
+        tags: tags,
+        steps: steps,
+        index: createIndex(),
+        createdDate: createDate(),
+        targetDate: targetDate,
+        type: 'collection',
+        progressStatus: 'processing',
+        fromCollection: fromCollection,
     }
 }
 
