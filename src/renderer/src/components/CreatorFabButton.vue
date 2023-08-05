09408ef94a01df7ce93b890211ef45ba0b74223e
bre97-web <by.sgatn3@gmail.com>
2023-08-05 15:01:38 +0800

    fix fab, now creator fab only show in router page of the allow list's range

--- src/renderer/src/components/CreatorFabButton.vue
+++ src/renderer/src/components/CreatorFabButton.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="z-50">
-        <md-fab lowered variant="primary" :label="props.label" @click="() => creatorDialog.open = true">
+        <md-fab v-show="AllowRouterList.includes(router.currentRoute.value.path)" lowered variant="primary" :label="props.label" @click="() => creatorDialog.open = true">
             <md-icon slot="icon" class="text-[var(--md-sys-color-on-primary)]">add</md-icon>
         </md-fab>
     </div>
 </template>
 
 <script setup lang="ts">
+import { getCreatorFabAllowRouterList } from '@/scripts/navigation';
 import { MDDialog } from '@/types/MDDialog';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const AllowRouterList = getCreatorFabAllowRouterList()
 
 const props = withDefaults(defineProps<{
     label?: string
