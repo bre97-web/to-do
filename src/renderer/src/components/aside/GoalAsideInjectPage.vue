a7594ec4723db2d1f83722dd92575c335114b420
Bre 97 <by.sgatn3@Gmail.com>
2023-09-03 15:17:50 +0800

    Update GoalAsideInjectPage.vue

--- src/renderer/src/components/aside/GoalAsideInjectPage.vue
+++ src/renderer/src/components/aside/GoalAsideInjectPage.vue
@@ -4,7 +4,6 @@
             <TitleLarge>{{ props.goal.goalSteps?.goals[props.goal.goalSteps.currentIndex].title }}</TitleLarge>
             <TitleMedium>{{ props.goal.goalSteps?.currentIndex || 0 + 1 }} / {{ props.goal.goalSteps?.maxIndex || 0 + 1 }}</TitleMedium>
         </div>
-
     </FlexColLayout>
 </template>
 
@@ -17,6 +16,4 @@ const props = defineProps<{
 }>()
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
